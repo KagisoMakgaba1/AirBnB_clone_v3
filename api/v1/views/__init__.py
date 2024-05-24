19a82470a21fa0ccbd95faf04a5de7770aa8363d
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-05-24 13:05:54 -0700

    __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-views
-"""
 
 from flask import Blueprint
 
@@ -15,3 +12,7 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
+
+"""
+views
+"""
