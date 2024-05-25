7ca50b1a0b3763a7523a1307a3e63ca713d65e1d
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-05-25 09:00:54 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''Contains a Flask web application API.
 '''
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
