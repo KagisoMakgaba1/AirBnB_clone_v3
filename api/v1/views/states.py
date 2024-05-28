7d791459e7d603a9f71b6f17adbbce47a0006ef7
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-05-28 06:08:57 -0700

    states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -33,14 +33,17 @@ def handle_states(state_id=None):
 def get_states(state_id=None):
     '''Gets the state with the given id or all states.
     '''
-    all_states = storage.all(State).values()
-    if state_id:
-        res = list(filter(lambda x: x.id == state_id, all_states))
-        if res:
-            return jsonify(res[0].to_dict())
-        raise NotFound()
-    all_states = list(map(lambda x: x.to_dict(), all_states))
-    return jsonify(all_states)
+    try:
+        all_states = storage.all(State).values()
+        if state_id:
+            res = list(filter(lambda x: x.id == state_id, all_states))
+            if res:
+                return jsonify(res[0].to_dict()), 200
+            raise NotFound()
+        all_states = list(map(lambda x: x.to_dict(), all_states))
+        return jsonify(all_states), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 def remove_state(state_id=None):
